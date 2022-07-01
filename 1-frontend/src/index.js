953d0dd93e1f2a9b07ccc5eabf19be4554b8d679
multi-vit <103585624+multi-vit@users.noreply.github.com>
2022-07-01 14:00:44 +0100

    Complete Calling an API that validates our JWT

--- 1-frontend/src/index.js
+++ 1-frontend/src/index.js
@@ -5,7 +5,6 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Auth0Provider } from "@auth0/auth0-react";
 
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
@@ -14,6 +13,8 @@ root.render(
       domain={process.env.REACT_APP_DOMAIN}
       clientId={process.env.REACT_APP_CLIENTID}
       redirectUri={window.location.origin}
+      audience={process.env.REACT_APP_AUDIENCE}
+      scope={process.env.REACT_APP_SCOPE}
     >
       <App />
     </Auth0Provider>
